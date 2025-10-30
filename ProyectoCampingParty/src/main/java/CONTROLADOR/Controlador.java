3b02cc5cc9062a6bf8bfe01198eefce86321d549
sergiogimenezgomez19-dev <sergiogimenezgomez19@gmail.com>
2025-10-30 13:13:03 +0100

    Controlador
    Controlador

--- ProyectoCampingParty/src/main/java/CONTROLADOR/Controlador.java
+++ ProyectoCampingParty/src/main/java/CONTROLADOR/Controlador.java
@@ -4,11 +4,57 @@
  */
 package CONTROLADOR;
 
+import MODELO.Actividad;
+import MODELO.Cliente;
+import MODELO.Modelo;
+import MODELO.Parcela;
+import MODELO.Participacion;
+import MODELO.Reserva;
+import MODELO.Tienda;
+import VISTA.ActividadSeleccionada;
+import VISTA.ClienteConReserva;
+import VISTA.ClienteLogLogin;
+import VISTA.ClienteLogin;
+import VISTA.ClienteSignOn;
+import VISTA.JDialogConfirmar;
+import VISTA.TrabajadorLogin;
+import VISTA.TrabajadorSesión;
+import VISTA.VistaCliente;
+import VISTA.VistaClienteModificarReserva;
+import VISTA.VistaClienteReserva;
+import VISTA.VistaLogin;
+import VISTA.VistaReservas;
+import VISTA.VistaStaffActividad;
+import VISTA.VistaStaffEntrada;
+
 /**
  *
  * @author Carla Terol
  */
 public class Controlador {
+
+private ActividadSeleccionada as;
+private ClienteLogLogin cll;
+private ClienteConReserva ccr;
+private ClienteLogin cl;
+private ClienteSignOn cso;
+private JDialogConfirmar jdc;
+private TrabajadorLogin tl;
+private TrabajadorSesión ts;
+private VistaCliente vc;
+private VistaClienteModificarReserva vcmr;
+private VistaClienteReserva vcr;
+private VistaLogin vl;
+private VistaReservas vr;
+private VistaStaffActividad vsa;
+private VistaStaffEntrada vse;
+private Modelo m;
+private Actividad a;
+private Cliente c;
+private Parcela p;
+private Participacion pa;
+private Reserva r;
+private Tienda t;
 
 
 
